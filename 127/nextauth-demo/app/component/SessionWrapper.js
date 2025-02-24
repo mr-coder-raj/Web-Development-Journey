e6feb5641fbaedd781f1a0877ef9098369254fd8
MrCoderRaj <rajr127655@gmail.com>
2025-02-25 01:03:35 +0530

    inital commit

---
+++ 127/nextauth-demo/app/component/SessionWrapper.js
@@ -0,0 +1,13 @@
+"use client"
+import { SessionProvider } from "next-auth/react"
+
+
+const SessionWrapper = ({ children }) => {
+  return (
+    <div>
+      <SessionProvider>{children}</SessionProvider>
+    </div>
+  )
+}
+
+export default SessionWrapper
